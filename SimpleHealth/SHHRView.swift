a6cea10d2c35d5aa2fa919db0cdc5a6c9aadcdcd
Richard Sprague <richardsprague@gmail.com>
2020-07-27 12:03:06 -0700

    Shows current age, pulled from Apple Health.

--- SimpleHealth/SHHRView.swift
+++ SimpleHealth/SHHRView.swift
@@ -9,19 +9,40 @@
 import SwiftUI
 
 struct SHHRView: View {
+    
+  
     var body: some View {
         VStack{
-        Text("Your Health Data").font(.title)
+            Text("Your Health Data" ).font(.title)
             ZStack{
                 Rectangle()
                     .fill(Color(.blue))
                     .frame(width:300, height:400)
                 .padding()
-                Text("Heart Rate").foregroundColor(.white)
+                Text("Heart Rate = " + String(self.getAge()))
+                    .foregroundColor(.white)
                 
             }
             }
         }
+    
+    private func getAge() -> Int {
+        
+        let userHealthProfile = UserHealthProfile()
+        
+        do {
+          let userAgeSexAndBloodType = try ProfileDataStore.getAgeSexAndBloodType()
+          userHealthProfile.age = userAgeSexAndBloodType.age
+          userHealthProfile.biologicalSex = userAgeSexAndBloodType.biologicalSex
+          userHealthProfile.bloodType = userAgeSexAndBloodType.bloodType
+
+        } catch let error {
+            print("Unable to read profile data (error:" + error.localizedDescription)
+        }
+
+        
+        return userHealthProfile.age ?? 0
+    }
 
 }
 
